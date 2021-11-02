8cfe23a4a28014006a1cdd7ed71439e649ca79fa
Esteban Elí Gómez González <estebangomezhachy@gmail.com>
2021-11-02 14:36:46 -0400

    Se crea el archivo bash y se añaden las primeras lineas de código

---
+++ administracion_de_notas_trimestre.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+declare -a Unix=();
+
+x=1
+echo 'Ingresa la cantidad evaluaciones realizadas en tu trimestre: '
+read cantidad_evaluaciones
+suma_notas=0
+notas=()
+nota=0
+
+while [ $x -le $cantidad_evaluaciones ]
+do
+    while [ $nota -lt 1 ] || [ $nota -gt 20 ]
+    do
+        echo "Ingresa la nota de la $x evaluación"
+        read nota
+    done    
+    notas=("${notas[@]}" $nota)
+    suma_notas=$(($suma_notas + $nota))
+    x=$(( $x + 1 ))
+    nota=0
+done
+
+x=0
+
+while [ $x -le $(($cantidad_evaluaciones - 1)) ]
+do
+    echo "La nota de la $(($x + 1)) evaluación es: ${notas[$x]}"
+    x=$(( $x + 1 ))
+done
+
+echo "Promedio: $(($suma_notas/$cantidad_evaluaciones))"
